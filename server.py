e679dbc44238e64c504e335775714cec786a5809
Jacobdocherty <jacob@oakandsteel.ca>
2025-03-18 07:23:33 -0400

    Add files via upload

--- server.py
+++ server.py
@@ -1,12 +1,13 @@
+
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Elevate Estimator is running!'
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 10000))  # Get correct port from Render
+    port = int(os.environ.get('PORT', 10000))  # Ensure the app binds to the correct port
     app.run(host='0.0.0.0', port=port)
